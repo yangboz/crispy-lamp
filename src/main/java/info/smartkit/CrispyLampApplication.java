66058bc8f8942fba2ceb8beb5e6a0b4d1ca9db02
yangboz <zhou.yangbo@intellif.com>
2016-02-16 10:27:21 +0800

    ADDED:spring-boot initializer related initial project files

---
+++ src/main/java/info/smartkit/CrispyLampApplication.java
@@ -0,0 +1,12 @@
+package info.smartkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrispyLampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrispyLampApplication.class, args);
+	}
+}
